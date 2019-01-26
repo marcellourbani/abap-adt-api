57ce06a216b505af98ff2bf0a8977adf879b172f
Marcello Urbani <marcello.urbani@gmail.com>
2019-01-26 00:44:04 +0000

    create (partial)

--- src/api/objectcreator.ts
+++ src/api/objectcreator.ts
@@ -1,43 +1,136 @@
 import { parse } from "fast-xml-parser"
+import { sprintf } from "sprintf-js"
 import { adtException } from "../AdtException"
 import { AdtHTTP } from "../AdtHTTP"
 import { xmlArray, xmlNodeAttr } from "../utilities"
 interface CreatableType {
   validationPath: string
+  creationPath: string
+  rootName: string
+  nameSpace: string
 }
-
-export type CreatableTypeIds =
-  | "PROG/P"
+type GroupTypeIds = "FUGR/FF" | "FUGR/I"
+export type NonGroupTypeIds =
   | "CLAS/OC"
-  | "INTF/OI"
   | "FUGR/F"
-  | "FUGR/FF"
+  | "INTF/OI"
   | "PROG/I"
-  | "FUGR/I"
+  | "PROG/P"
+export type CreatableTypeIds = GroupTypeIds | NonGroupTypeIds
 
 export const CreatableTypes: Map<CreatableTypeIds, CreatableType> = new Map([
-  ["PROG/P", { validationPath: "programs/validation" }],
-  ["CLAS/OC", { validationPath: "oo/validation/objectname" }],
-  ["INTF/OI", { validationPath: "oo/validation/objectname" }],
-  ["FUGR/F", { validationPath: "functions/validation" }],
-  ["FUGR/FF", { validationPath: "functions/validation" }],
-  ["PROG/I", { validationPath: "includes/validation" }],
-  ["FUGR/I", { validationPath: "functions/validation" }]
+  [
+    "PROG/P",
+    {
+      creationPath: "programs/programs",
+      nameSpace: 'xmlns:program="http://www.sap.com/adt/programs/programs"',
+      rootName: "program:abapProgram",
+      validationPath: "programs/validation"
+    }
+  ],
+  [
+    "CLAS/OC",
+    {
+      creationPath: "oo/classes",
+      nameSpace: 'xmlns:class="http://www.sap.com/adt/oo/classes"',
+      rootName: "class:abapClass",
+      validationPath: "oo/validation/objectname"
+    }
+  ],
+  [
+    "INTF/OI",
+    {
+      creationPath: "oo/interfaces",
+      nameSpace: 'xmlns:intf="http://www.sap.com/adt/oo/interfaces',
+      rootName: "intf:abapInterface",
+      validationPath: "oo/validation/objectname"
+    }
+  ],
+  [
+    "PROG/I",
+    {
+      creationPath: "programs/includes",
+      nameSpace: 'xmlns:include="http://www.sap.com/adt/programs/includes"',
+      rootName: "include:abapInclude",
+      validationPath: "includes/validation"
+    }
+  ],
+  [
+    "FUGR/F",
+    {
+      creationPath: "/sap/bc/adt/functions/groups",
+      nameSpace: 'xmlns:group="http://www.sap.com/adt/functions/groups"',
+      rootName: "group:abapFunctionGroup",
+      validationPath: "functions/validation"
+    }
+  ],
+  [
+    "FUGR/FF",
+    {
+      creationPath: "functions/groups/%s/fmodules",
+      nameSpace: 'xmlns:fmodule="http://www.sap.com/adt/functions/fmodules"',
+      rootName: "fmodule:abapFunctionModule",
+      validationPath: "functions/validation"
+    }
+  ],
+  [
+    "FUGR/I",
+    {
+      creationPath: "functions/groups/%s/includes",
+      nameSpace: 'xmlns:finclude="http://www.sap.com/adt/functions/fincludes"',
+      rootName: "finclude:abapFunctionGroupInclude",
+      validationPath: "functions/validation"
+    }
+  ]
 ]) as Map<CreatableTypeIds, CreatableType>
 
 interface ObjectValidateOptions {
-  objtype: CreatableTypeIds
+  objtype: NonGroupTypeIds
   objname: string
   packagename: string
   description: string
 }
 interface GroupValidateOptions {
-  objtype: CreatableTypeIds
+  objtype: GroupTypeIds
   objname: string
   fugrname: string
   description: string
 }
 export type ValidateOptions = ObjectValidateOptions | GroupValidateOptions
+export interface NewObjectOptions {
+  objtype: CreatableTypeIds
+  name: string
+  parentName: string
+  description: string
+  devclass: string
+  parentPath: string
+  responsible?: string
+  transport?: string
+}
+
+function createBody(options: NewObjectOptions, type: CreatableType) {
+  if (options.objtype === "FUGR/FF" || options.objtype === "FUGR/I") {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <${type.rootName} ${type.nameSpace}
+       xmlns:adtcore="http://www.sap.com/adt/core"
+       adtcore:description="${options.description}"
+       adtcore:name="${options.name}" adtcore:type="${options.objtype}"
+       adtcore:responsible="${options.responsible}">
+         <adtcore:containerRef adtcore:name="${options.parentName}"
+           adtcore:type="FUGR/F"
+           adtcore:uri="${options.parentPath}"/>
+    </${type.rootName}>`
+  } else {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <${type.rootName} ${type.nameSpace}
+      xmlns:adtcore="http://www.sap.com/adt/core"
+      adtcore:description="${options.description}"
+      adtcore:name="${options.name}" adtcore:type="${options.objtype}"
+      adtcore:responsible="${options.responsible}">
+        <adtcore:packageRef adtcore:name="${options.parentName}"/>
+    </${type.rootName}>`
+  }
+}
 
 export async function loadTypes(h: AdtHTTP) {
   const response = await h.request("/sap/bc/adt/repository/typestructure", {
@@ -55,6 +148,7 @@ export async function loadTypes(h: AdtHTTP) {
     "SEU_ADT_OBJECT_TYPE_DESCRIPTOR"
   ).map(xmlNodeAttr)
 }
+
 export async function validate(h: AdtHTTP, options: ValidateOptions) {
   const ot = CreatableTypes.get(options.objtype)
   if (!ot) throw adtException("Unsupported object type")
@@ -68,4 +162,26 @@ export async function validate(h: AdtHTTP, options: ValidateOptions) {
   })
   const record = xmlArray(raw, "asx:abap", "asx:values", "DATA") as any[]
   return !!(record[0] && record[0].CHECK_RESULT === "X")
+}
+
+export async function create(h: AdtHTTP, options: NewObjectOptions) {
+  const ot = CreatableTypes.get(options.objtype)
+  if (!ot) throw adtException("Unsupported object type")
+  const url = "/sap/bc/adt/" + sprintf("/sap/bc/adt/", options.parentName)
+  const data = createBody(options, ot)
+  const params: any = {}
+  if (options.transport) params.corrNr = options.transport
+
+  const response = await h.request(url, {
+    data,
+    method: "POST",
+    params
+  })
+  const raw = parse(response.data, {
+    ignoreAttributes: false,
+    parseAttributeValue: true
+  })
+  // const record = xmlArray(raw, "asx:abap", "asx:values", "DATA") as any[]
+  // return !!(record[0] && record[0].CHECK_RESULT === "X")
+  return raw
 }
