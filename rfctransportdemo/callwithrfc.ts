6fb7423f5f2843f8e935f1bcb17650f6626deaaf
Marcello Urbani <marcello.urbani@gmail.com>
2023-10-28 13:13:05 +0100

    improved RFC client

--- rfctransportdemo/callwithrfc.ts
+++ rfctransportdemo/callwithrfc.ts
@@ -5,6 +5,7 @@ import { Client, RfcConnectionParameters, RfcObject } from "node-rfc"
 import { ADTClient, HttpClient } from "abap-adt-api";
 import { HttpClientOptions, HttpClientResponse } from "../build/AdtHTTP"
 
+const CSRF_TOKEN_HEADER = "x-csrf-token"
 interface RfcHeader extends RfcObject {
     NAME: string
     VALUE: string
@@ -38,28 +39,26 @@ const getRfcParameters = (): RfcConnectionParameters | undefined => {
     if (ASHOST && SYSNR && USER && PASSWD) return { ASHOST, SYSNR, USER, PASSWD }
 }
 
-const createClient = () => {
-    const params = getRfcParameters()
-    if (params) return new Client(params)
-}
-
-
 class RfcHttpClient implements HttpClient {
     constructor(private client: Client) { }
     async request(options: HttpClientOptions): Promise<HttpClientResponse> {
         const HEADER_FIELDS = Object.keys(options?.headers || {}).map(NAME => ({ NAME, VALUE: options?.headers?.[NAME] || "" }))
+        const qs = options.qs || {}
+        const query = Object.keys(qs).map(k => `${k}=${qs[k]}`).join("&")
+        const URI = query ? `${options.url}?${query}` : options.url
         const REQUEST: RfcRequest = {
             HEADER_FIELDS,
             MESSAGE_BODY: Buffer.from(""),
             REQUEST_LINE: {
                 METHOD: options.method || "GET",
-                URI: options.url,
+                URI,
                 VERSION: ""
             }
         }
         const resp = await this.client.call("SADT_REST_RFC_ENDPOINT", { REQUEST })
         const { STATUS_LINE, HEADER_FIELDS: respfields, MESSAGE_BODY } = resp.RESPONSE as RfcResponse
         const respheaders: Record<string, string> = respfields.reduce((acc, h) => { acc[h.NAME] = h.VALUE; return acc }, {} as Record<string, string>)
+        respheaders[CSRF_TOKEN_HEADER] ||= "notneededoverrfc"
         return { body: `${MESSAGE_BODY}`, headers: respheaders, status: parseInt(STATUS_LINE.STATUS_CODE), statusText: STATUS_LINE.REASON_PHRASE }
     }
 }
@@ -75,14 +74,21 @@ const main = async () => {
         await client.open()
 
         const rfcClient = new RfcHttpClient(client)
+        const adtClient = new ADTClient(rfcClient, params.USER || "", "")
+
         // Test direct HTTP call
         const resp = await rfcClient.request({ url: "/sap/bc/adt/programs/programs/SAPMCRFC", headers: { Accept: "*/*" } })
         console.log(`Direct HTTP call:\n`, resp)
 
         // test client
-        const adtClient = new ADTClient(rfcClient, params.USER || "", "")
         const struct = await adtClient.objectStructure("/sap/bc/adt/programs/programs/SAPMCRFC")
         console.log(`\nClient API call:\n`, struct)
+
+        const ref = await adtClient.transportReference('LIMU', 'FUNC', 'SADT_REST_RFC_ENDPOINT')
+        console.log(`\nClient API call with query parameters:\n`, ref)
+
+        const nodec = await adtClient.nodeContents('FUGR/F', "SADT_REST")
+        console.log(`\nClient API call with post:\n`, nodec)
 
     } catch (error) {
         console.log(error)
